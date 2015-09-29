5581d329930934871f67bb66c8c4b5e0b63a155e
Joshua Ugba <justjosh001@gmail.com>
2015-09-29 17:04:49 +0100

    Add ShardedBuffer to hold log buffer and reactant buffer

---
+++ src/checkpoint/andela/parser/SharedBuffers.java
@@ -0,0 +1,21 @@
+package checkpoint.andela.parser;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+public class SharedBuffers {
+	public static BlockingQueue<String> logBuffer = new LinkedBlockingQueue<>();
+	public static BlockingQueue<Reactant> sharedBuffer = new LinkedBlockingQueue<>();
+	public SharedBuffers() {
+	}
+
+	public static BlockingQueue<String> getLogBuffer() {
+		return logBuffer;
+	}
+	
+	public static BlockingQueue<Reactant> getSharedBuffer() {
+		return sharedBuffer;
+	}	
+
+}
