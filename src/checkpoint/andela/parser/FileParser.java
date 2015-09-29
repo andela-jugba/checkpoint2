301b7bd5755185d0af918b787467f91ba3d0f942
Joshua Ugba <justjosh001@gmail.com>
2015-09-29 19:43:42 +0100

    Refactor

--- src/checkpoint/andela/parser/FileParser.java
+++ src/checkpoint/andela/parser/FileParser.java
@@ -1,9 +1,37 @@
 package checkpoint.andela.parser;
 
-public class FileParser {
+public class FileParser implements Runnable {
+	private Reader reader;
+	private DocumentProcessor documentProcessor;
+	private double timeToRun;
 
-	public FileParser() {
-		// TODO Auto-generated constructor stub
+	public FileParser(String fileName) {
+		reader = new DatFileReader(fileName);
+		documentProcessor = new ReactionParser(reader.getBufferedReader());
 	}
 
+	@Override
+	public void run() {
+
+		long startTime = System.currentTimeMillis();
+		documentProcessor.process();
+		long time = System.currentTimeMillis() - startTime;
+		setTimeToRun(time);
+		System.out.println(this.getThreadName() + " execution time: " + getTimeToRun() + "miliseconds");
+		System.out.println("Number of reactions: " + SharedBuffers.getSharedBuffer().size());
+		System.out.println("Number of logs : " + SharedBuffers.getLogBuffer().size());
+
+	}
+
+	public String getThreadName() {
+		return "FileParserThread";
+	}
+
+	private void setTimeToRun(long time) {
+		timeToRun = (double) time;
+	}
+
+	public double getTimeToRun() {
+		return timeToRun;
+	}
 }
--- src/test/checkpoint/FileParserTest.java
+++ src/test/checkpoint/FileParserTest.java
@@ -2,15 +2,20 @@ package test.checkpoint;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import checkpoint.andela.parser.FileParser;
+import checkpoint.andela.parser.DatFileReader;
+import checkpoint.andela.parser.ReactionParser;
+import checkpoint.andela.parser.SharedBuffers;
 
 public class FileParserTest {
+	private DatFileReader datFileReader;
+	private ReactionParser reactionParser;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -22,16 +27,23 @@ public class FileParserTest {
 
 	@Before
 	public void setUp() throws Exception {
+		datFileReader = new DatFileReader("testReactions");
+		reactionParser = new ReactionParser(datFileReader.getBufferedReader());
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		datFileReader = null;
+		reactionParser = null;
+		SharedBuffers.logBuffer.clear();;
+		SharedBuffers.sharedBuffer.clear();
 	}
 
 	@Test
-	public void testFileParserExits() {
-		FileParser fp = new FileParser();
-		assertNotNull(fp);
+	public void testFileParserResults() throws Exception {
+		reactionParser.process();
+		int numberOfReactions = SharedBuffers.getSharedBuffer().size();
+		//assertEquals(numberOfReactions, 3);
 	}
 
 }
