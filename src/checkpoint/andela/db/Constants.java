3afc38c2adfd51dc3375d58a540d1bd3040c71a9
Joshua Ugba <justjosh001@gmail.com>
2015-10-06 13:57:36 +0100

    Create constant class

---
+++ src/checkpoint/andela/db/Constants.java
@@ -0,0 +1,7 @@
+package checkpoint.andela.db;
+
+public class Constants {
+	public static String DATABASEURL = "jdbc:mysql://localhost/reactiondb";
+	public static String USERNAME = "andela_jugba";
+	public static String PASSWORD = "justjosh";
+}
